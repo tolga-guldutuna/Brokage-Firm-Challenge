a540c767fda2cd29cea94577fe07d4c2bf618ff8
Tolga Güldütuna <33281710+tolga-guldutuna@users.noreply.github.com>
2024-10-07 22:07:14 +0300

    proje bitmiştir

--- src/main/java/com/brokenFirmChallenge/Application.java
+++ src/main/java/com/brokenFirmChallenge/Application.java
@@ -7,9 +7,12 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 
 import java.awt.Desktop;
 import java.net.URI;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Application implements ApplicationListener<ApplicationReadyEvent> {
+
+	private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -18,11 +21,20 @@ public class Application implements ApplicationListener<ApplicationReadyEvent> {
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		try {
-			String url = "http://localhost:8585/OrderManagement/swagger-ui/index.html";
+			String url = "http://localhost:8585/Brokage-Firm-Challenge/swagger-ui/index.html";
 			if (Desktop.isDesktopSupported()) {
-				Desktop.getDesktop().browse(new URI(url));
+				Desktop desktop = Desktop.getDesktop();
+				if (desktop.isSupported(Desktop.Action.BROWSE)) {
+					desktop.browse(new URI(url));
+					LOGGER.info("Swagger UI opened successfully in the browser.");
+				} else {
+					LOGGER.warning("BROWSE action is not supported on this system.");
+				}
+			} else {
+				LOGGER.warning("Desktop is not supported on this system.");
 			}
 		} catch (Exception e) {
+			LOGGER.severe("Failed to open Swagger UI: " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
